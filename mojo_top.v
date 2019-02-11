60256aa73c24662409fae1df7580c48ab9329d1a
Kevin Balke <fughilli@gmail.com>
2019-02-10 22:17:54 -0800

    Added SPI slave to top-level module

--- mojo_top.v
+++ mojo_top.v
@@ -22,6 +22,85 @@ module mojo_top (
     .out(rst)
   );
 
+  reg mem_clka;
+  wire mem_wea;
+  reg[12:0] mem_addra;
+  wire[7:0] mem_dina;
+  wire[7:0] mem_douta;
+
+  reg mem_clkb;
+  wire mem_web;
+  reg[12:0] mem_addrb;
+  reg[7:0] mem_dinb;
+  wire[7:0] mem_doutb;
+
+  parameter MEMORY_STATE_WRITE_ADDRESS = 0;
+  parameter MEMORY_STATE_WRITE_DATA = 1;
+  reg[2:0] memory_address_position;
+  reg[2:0] memory_state;
+
+  blk_mem_gen_v7_3(.clka(mem_clk_a), .rsta(rst), .wea(mem_wea),
+                   .addra(mem_addra), .dina(mem_dina), .douta(mem_douta),
+                   .clkb(mem_clkb), .rstb(rst), .web(mem_web),
+                   .addrb(mem_addrb), .doutb(mem_doutb));
+
+  // Always write disable port A (port A is used to drive the strips).
+  assign mem_wea = 0;
+  // Not writing; terminate DINA as 0.
+  assign mem_dina = 8'h00;
+  // Always write enable port B (port B is driven by the SPI slave).
+  assign mem_web = 1;
+
+  wire[7:0] spi_din;
+  wire spi_done;
+  wire[7:0] spi_dout;
+
+  spi_slave spi_out (
+    .clk(clk),
+    .din(spi_din),
+    .done(spi_done),
+    .dout(spi_dout),
+    .miso(spi_miso),
+    .mosi(spi_mosi),
+    .rst(rst),
+    .sck(spi_clk),
+    .ss(spi_ss)
+  );
+
+  wire spi_selected = ~spi_ss;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      mem_clka <= 0;
+      mem_addra <= 13'h0000;
+      mem_clkb <= 0;
+      mem_addrb <= 13'h0000;
+      mem_dinb <= 8'h00;
+      memory_address_position = 0;
+    end else begin
+      if (spi_selected) begin
+        if (memory_state == MEMORY_STATE_WRITE_ADDRESS) begin
+          if (spi_done) begin
+            mem_addrb <= {mem_addrb[4:0], spi_dout};
+            memory_address_position <= memory_address_position + 1;
+
+            if (memory_address_position == 1) begin
+              memory_state == MEMORY_STATE_WRITE_DATA;
+            end
+          end
+        end else if (memory_state == MEMORY_STATE_WRITE_DATA) begin
+          spi_din <= mem_doutb;
+          mem_dinb <= spi_dout;
+          mem_addrb <= mem_addrb + 1;
+          mem_clkb <= spi_done;
+        end
+      end else begin
+        mem_clkb <= 0;
+        memory_state <= MEMORY_STATE_WRITE_ADDRESS;
+      end
+    end
+  end
+
   parameter NUM_LEDS = 160;
   parameter TOTAL_PULSE_TIME = 70;
   parameter ZERO_PULSE_TIME = 20;
