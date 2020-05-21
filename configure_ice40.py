83790464fcc07c2d7e064469b7339c0ae08fd611
Kevin Balke <fughilli@gmail.com>
2020-05-20 20:23:55 -0700

    Added license file

--- configure_ice40.py
+++ configure_ice40.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python
 
+# LED Suit FPGA Implementation - Multi-output addressable LED driver HDL
+# implementation for Kevin's LED suit controller.
+# Copyright (C) 2019-2020 Kevin Balke
+#
+# This file is part of LED Suit FPGA Implementation.
+#
+# LED Suit FPGA Implementation is free software: you can redistribute it and/or
+# modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LED Suit FPGA Implementation is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LED Suit FPGA Implementation.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+
 from ninja_syntax import Writer
 import os
 import sys
--- test_strip_driver.v
+++ test_strip_driver.v
@@ -1,3 +1,26 @@
+//
+// LED Suit FPGA Implementation - Multi-output addressable LED driver HDL
+// implementation for Kevin's LED suit controller.
+// Copyright (C) 2019-2020 Kevin Balke
+//
+// This file is part of LED Suit FPGA Implementation.
+//
+// LED Suit FPGA Implementation is free software: you can redistribute it and/or
+// modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LED Suit FPGA Implementation is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with LED Suit FPGA Implementation.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
 
 `timescale 10 ns / 1 ns
 
